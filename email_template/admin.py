eb5c87be0212dbbcf8c1386966f08a2924cac516
Polina Koval <polina.koval@anvileight.com>
2022-11-22 20:15:29 +0300

    Add save_as to EmailTemplateAdmin.

--- email_template/admin.py
+++ email_template/admin.py
@@ -6,4 +6,4 @@ from email_template.models import EmailTemplate
 @admin.register(EmailTemplate)
 class EmailTemplateAdmin(admin.ModelAdmin):
     list_display = ["template_key", "subject", "from_email", "to_email"]
-
+    save_as = True
