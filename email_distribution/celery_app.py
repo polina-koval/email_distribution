05b89423ac181bd97750ae13f84970730f61d559
Polina Koval <polina.koval@anvileight.com>
2022-11-23 14:47:06 +0300

    Create periodic task in celery.

--- email_distribution/celery_app.py
+++ email_distribution/celery_app.py
@@ -1,9 +1,16 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTING_MODULE", "email_distribution.settings")
 app = Celery("email_distribution")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
+app.conf.beat_schedule = {
+    'send-email-every-5-minute': {
+        'task': 'users.tasks.delayed_send_email',
+        'schedule': crontab(minute='*/5')
+    }
+}
--- users/tasks.py
+++ users/tasks.py
@@ -1,7 +1,21 @@
 from email_distribution.celery_app import app
 from email_template.models import EmailTemplate
+from users.models import User
 
 
 @app.task()
 def send_email(*args, **kwargs):
-    return EmailTemplate.send(*args, **kwargs)
+    EmailTemplate.send(*args, **kwargs)
+
+
+@app.task()
+def delayed_send_email():
+    for user in User.objects.all():
+        template_name = "test"
+        emails = [user.email]
+        context = {"value_1": 'hello'}
+        EmailTemplate.send(
+            template_name,
+            context,
+            emails=emails,
+        )
