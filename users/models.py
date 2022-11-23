8b1ce38de4c9e43afa6f148a1fd23447e137d5c8
Polina Koval <polina.koval@anvileight.com>
2022-11-23 20:34:46 +0300

    Add Email model.

--- users/models.py
+++ users/models.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+
+from email_template.models import EmailTemplate
 
 
 class User(AbstractUser):
     """Default user for Email Distribution."""
 
     #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
 
@@ -20,3 +22,18 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"username": self.username})
+
+
+class Email(models.Model):
+    template = models.ForeignKey(
+        EmailTemplate,
+        on_delete=models.CASCADE,
+        related_name="emails_from_template",
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="emails_from_user"
+    )
+    is_open = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.name or self.user.username
